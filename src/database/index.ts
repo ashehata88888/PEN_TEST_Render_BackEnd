90ee973adde039bd1ba1ff28799122d64c4a7d98
ashehata8888 <133015772+ashehata88888@users.noreply.github.com>
2023-05-11 10:41:51 +0200

    some changes in db credentials

--- src/database/index.ts
+++ src/database/index.ts
@@ -16,27 +16,38 @@ const {
 
 let db: Pool
 
-if (ENV === 'dev') {
-  db = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB_TEST,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    port: parseInt(POSTGRES_PORT as string),
-    max: parseInt(POSTGRES_MAX as string)
-  })
-}
 
-if (ENV === 'test') {
-  db = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB_TEST,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    port: parseInt(POSTGRES_PORT as string),
-    max: parseInt(POSTGRES_MAX as string)
-  })
-}
+db = new Pool({
+  host: POSTGRES_HOST,
+  database: POSTGRES_DB_TEST,
+  user: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  port: parseInt(POSTGRES_PORT as string),
+  max: parseInt(POSTGRES_MAX as string)
+})
+
+
+// if (ENV === 'dev') {
+//   db = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_DB_TEST,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//     port: parseInt(POSTGRES_PORT as string),
+//     max: parseInt(POSTGRES_MAX as string)
+//   })
+// }
+
+// if (ENV === 'test') {
+//   db = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_DB_TEST,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//     port: parseInt(POSTGRES_PORT as string),
+//     max: parseInt(POSTGRES_MAX as string)
+//   })
+// }
 
 db.on('error', (error: Error) => {
   console.error(error.message)
