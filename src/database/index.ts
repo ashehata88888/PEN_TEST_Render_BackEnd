360d7a2ed0073344415fd02e54f136a521c18b84
ashehata8888 <133015772+ashehata88888@users.noreply.github.com>
2023-05-11 10:54:59 +0200

    db connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -16,15 +16,24 @@ const {
 
 let db: Pool
 
-
 db = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB_TEST,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-  port: parseInt(POSTGRES_PORT as string),
-  max: parseInt(POSTGRES_MAX as string)
+  host: "dpg-chdm8p2k728nnn0t1g0g-a",
+  database: "pen_test_render",
+  user: "postgres_render",
+  password: "YXSs2ETjRWUIqJR2alVouWGrbvbTu7mz",
+  port: 5432,
+  max: 6
 })
+
+
+// db = new Pool({
+//   host: POSTGRES_HOST,
+//   database: POSTGRES_DB_TEST,
+//   user: POSTGRES_USER,
+//   password: POSTGRES_PASSWORD,
+//   port: parseInt(POSTGRES_PORT as string),
+//   max: parseInt(POSTGRES_MAX as string)
+// })
 
 
 // if (ENV === 'dev') {
